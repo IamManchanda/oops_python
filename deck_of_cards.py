e685ae9e148520728956f5eea64db65ef222f5a8
Harry Manchanda <harmanmanchanda182@gmail.com>
2019-06-21 18:20:55 +0530

    Working pass on deck of cards!

---
+++ deck_of_cards.py
@@ -0,0 +1,58 @@
+""" Python classes """
+
+from random import shuffle
+
+class Card:
+    def __init__(self, value, suit):
+        self.value = value
+        self.suit = suit
+
+    def __repr__(self):
+        return f"{self.value} of {self.suit}"
+
+class Deck:
+    def __init__(self):
+        values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]
+        suits = ["Hearts", "Diamonds", "Clubs", "Spades"]
+        self.cards = [Card(value, suit) for suit in suits for value in values]
+
+    def __repr__(self):
+        return f"This is a Deck of {self.count()} cards"
+
+    def count(self):
+        return len(self.cards)
+
+    def _deal(self, num):
+        count = self.count()
+        min_count_num = min([count, num])
+        if count == 0:
+            raise ValueError("All cards have been dealt")
+        else:
+            print(f"Going to remove {min_count_num} cards")
+        cards = self.cards[-min_count_num:]
+        self.cards = self.cards[:-min_count_num]
+        return cards
+
+    def deal_card(self):
+        return self._deal(1)[0]
+
+    def deal_hand(self, hand_size):
+        return self._deal(hand_size)
+
+    def shuffle(self):
+        if (self.count() < 52):
+            raise ValueError("Only full decks can be shuffled")
+        shuffle(self.cards)
+
+d = Deck()
+print(f"{d}\n")
+d.shuffle()
+print(f"Shuffled Cards: {d.cards}\n")
+
+card = d.deal_card()
+print(f"Removed Card: {card}\n")
+
+hand = d.deal_hand(5)
+print(f"Removed Cards: {hand}\n")
+
+print(f"Left Cards: {d.cards}")
